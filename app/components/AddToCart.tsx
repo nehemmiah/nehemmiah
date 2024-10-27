7f6d76abb350a14d006da44d5c57aa9eac728f7a
Angga Haryokusumo <anggaharyokusumo@gmail.com>
2024-10-27 23:19:06 +0700

    finished react

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const AddToCart = () => {
   return (
     <div>
-        <button onClick={() => console.log('Click')}>Add to Cart</button>
+        <button className='btn btn-accent' onClick={() => console.log('Click')}>Add to Cart</button>
     </div>
   )
 }
